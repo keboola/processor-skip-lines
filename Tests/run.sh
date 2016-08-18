cc4c6a59d1ad355c1b10ccca3c2ec89450776847
Odin <odinuv@gmail.com>
2016-08-18 11:27:48 +0200

    fix: parenthesis

--- Tests/run.sh
+++ Tests/run.sh
@@ -4,7 +4,7 @@ echo "Running tests"
 
 /code/run.sh
 
-cmp -s $KBC_DATADIR/out/tables/radio.csv /code/Tests/sample/radio.csv || echo "radio is different" && exit 1
-cmp -s $KBC_DATADIR/out/tables/text.csv /code/Tests/sample/text.csv || echo "text is different" && exit 1
+cmp -s $KBC_DATADIR/out/tables/radio.csv /code/Tests/sample/radio.csv || (echo "radio is different" && exit 1)
+cmp -s $KBC_DATADIR/out/tables/text.csv /code/Tests/sample/text.csv || (echo "text is different" && exit 1)
 
 echo "Tests finished"

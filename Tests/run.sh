89f7a9cb270db789b8f57164541c885c0e416569
Odin <odinuv@gmail.com>
2016-08-18 11:21:54 +0200

    fix: add tests messages

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Running tests"
+
 /code/run.sh
 
 cmp -s $KBC_DATADIR/out/tables/radio.csv /code/Tests/sample/radio.csv || echo "radio is different" && exit 1
 cmp -s $KBC_DATADIR/out/tables/text.csv /code/Tests/sample/text.csv || echo "text is different" && exit 1
+
+echo "Tests finished"

24f4ae5d35ba6a7171030334e4c7073c4a2ea815
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-14 16:08:45 +0100

    fix: add 1 to KBC_PARAMETER_LINES

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@
 cd $KBC_DATADIR/in/files/
 # create folders
 find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} sh -c "mkdir $KBC_DATADIR/out/files/\"{}\""
+# process files
+find . ! -iname "*.manifest" ! -name "." -type f | xargs -n1 -I {} sh -c "tail -n +$(expr $KBC_PARAMETER_LINES + 1) \"{}\" > $KBC_DATADIR/out/files/\"{}\""

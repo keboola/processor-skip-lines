4816e70455e5f150e09cb6078c14fedae6576ae6
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-14 16:08:31 +0100

    fix: create subfolders

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd $KBC_DATADIR/in/files/
-find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "tail -n +$KBC_PARAMETER_LINES \"{}\" > $KBC_DATADIR/out/files/\"{}\""
+# create folders
+find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} sh -c "mkdir $KBC_DATADIR/out/files/\"{}\""

72e876c09bac6292decd362a2c44e75dfab0567b
Odin <odinuv@gmail.com>
2018-03-27 16:05:51 +0200

    fix: maintain deep structure

--- src/processFile.php
+++ src/processFile.php
@@ -18,14 +18,12 @@ function processFile(\SplFileInfo $sourceFile, $destinationFolder, array $parame
         $slicedFiles = new Finder();
         $slicedFiles->files()->in($sourceFile->getPathname());
         $slicedDestination = $destinationFolder . '/' . $sourceFile->getFilename() . '/';
-        if (!$fs->exists($slicedDestination)) {
-            $fs->mkdir($slicedDestination);
-        }
         foreach ($slicedFiles as $slicedFile) {
+            $fs->mkdir($slicedDestination . $slicedFile->getRelativePath());
             if ($parameters['direction_from'] === 'bottom') {
-                $copyCommand = "head -n -" . $parameters["lines"] . " " . escapeshellarg($slicedFile->getPathname()) . " > " . escapeshellarg($slicedDestination . "/" . $slicedFile->getBasename());
+                $copyCommand = "head -n -" . $parameters["lines"] . " " . escapeshellarg($slicedFile->getPathname()) . " > " . escapeshellarg($slicedDestination . $slicedFile->getRelativePathname());
             } else {
-                $copyCommand = "tail -n +" . ($parameters["lines"] + 1) . " " . escapeshellarg($slicedFile->getPathname()) . " > " . escapeshellarg($slicedDestination . "/" . $slicedFile->getBasename());
+                $copyCommand = "tail -n +" . ($parameters["lines"] + 1) . " " . escapeshellarg($slicedFile->getPathname()) . " > " . escapeshellarg($slicedDestination . $slicedFile->getRelativePathname());
             }
 
             (new Process($copyCommand))->mustRun();

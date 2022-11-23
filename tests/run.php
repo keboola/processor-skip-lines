86bbbd80119a59f35a59aa9ef8b70e8ca68c7e21
Výborný Adam <mail@adamvyborny.cz>
2022-11-23 13:12:27 +0100

    Simplify FQN

--- tests/run.php
+++ tests/run.php
@@ -2,28 +2,33 @@
 
 declare(strict_types=1);
 
+use Keboola\Temp\Temp;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\Process;
+
 require_once(__DIR__ . "/../vendor/autoload.php");
 
 $testFolder = __DIR__;
 
-$finder = new \Symfony\Component\Finder\Finder();
+$finder = new Finder();
 $finder->directories()->sortByName()->in([$testFolder . '/top', $testFolder . '/bottom'])->depth(0);
-$fs = new \Symfony\Component\Filesystem\Filesystem();
+$fs = new Filesystem();
 
 foreach ($finder as $testSuite) {
     print "Test " . $testSuite->getPathname() . "\n";
-    $temp = new \Keboola\Temp\Temp("processor-select-columns");
+    $temp = new Temp("processor-select-columns");
     $temp->initRunFolder();
 
     $copyCommand = "cp -R " . $testSuite->getPathname() . "/source/data/* " . $temp->getTmpFolder();
-    \Symfony\Component\Process\Process::fromShellCommandline($copyCommand)->mustRun();
+    Process::fromShellCommandline($copyCommand)->mustRun();
 
     $fs->mkdir($temp->getTmpFolder() . "/out/tables", 0777);
     $fs->mkdir($temp->getTmpFolder() . "/out/files", 0777);
 
     $runCommand = "export KBC_DATADIR=\"{$temp->getTmpFolder()}\" && php /code/src/run.php --data=" .
         $temp->getTmpFolder();
-    $runProcess = \Symfony\Component\Process\Process::fromShellCommandline($runCommand);
+    $runProcess = Process::fromShellCommandline($runCommand);
     $runProcess->run();
 
     // detect errors
@@ -55,7 +60,7 @@ foreach ($finder as $testSuite) {
 
     $diffCommand = "diff --exclude=.gitkeep --ignore-all-space --recursive " .
         $testSuite->getPathname() . "/expected/data/out " . $temp->getTmpFolder() . "/out";
-    $diffProcess = \Symfony\Component\Process\Process::fromShellCommandline($diffCommand);
+    $diffProcess = Process::fromShellCommandline($diffCommand);
     $diffProcess->run();
     if ($diffProcess->getExitCode() > 0) {
         if ($diffProcess->getOutput()) {
